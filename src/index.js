5d771a51006fb5d87b696ca56b5775f9b96619f4
Saloni <salonimehta2127@gmail.com>
2021-05-15 22:56:43 -0400

    Added delete and addtasks functionality in todo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+const forms= document.getElementById("create-task-form");
+forms.addEventListener("submit",(e)=>{
+  e.preventDefault();
+  addAndDelete();   
+})
+const getList=document.getElementById("tasks");
+const input=document.getElementById("new-task-description");
+
+function addAndDelete()
+{
+  //add
+  const newLi=document.createElement("li");
+    getList.appendChild(newLi);
+    newLi.innerHTML=input.value;
+//delete
+  const del=document.createElement("button");
+    newLi.appendChild(del);
+    const addX=document.createTextNode("DONE");
+    del.appendChild(addX);
+    del.addEventListener("click",(e)=>{
+    e.target.parentElement.remove();
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
